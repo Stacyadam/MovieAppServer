c4be45e79bca6bef9e3b7fc530cec0f61b30e194
Stacy Adam <Stacy@portalmedia.com>
2018-12-21 21:42:08 -0600

    hooks up new postgres instance

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-	host: 'ec2-54-204-46-60.compute-1.amazonaws.com',
+	host: 'ec2-107-20-237-78.compute-1.amazonaws.com',
 	dialect: 'postgres',
 	dialectOptions: {
 		ssl: true
